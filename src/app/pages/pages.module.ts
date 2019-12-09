4726e203cf55540048d220a4ae5812a2e771c2a8
Ismael Arias <ismaeljarias@gmail.com>
2019-12-09 12:15:06 -0400

    Promise and RxJs completado

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -16,6 +16,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 
 // Temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
     Graficas1Component,
     IncrementadorComponent,
     GraficoDonaComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent
   ],
   exports: [
     PagesComponent,
