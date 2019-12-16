735b04983ef3297977d7ebc693f8dbcd8e296810
Ismael Arias <ismaeljarias@gmail.com>
2019-12-16 15:03:56 -0400

    Frontend login complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ServiceModule } from './services/service.module';
 
@@ -25,6 +25,7 @@ import { RegisterComponent } from './login/register.component';
     AppRoutingModule,
     PagesModule,
     FormsModule,
+    ReactiveFormsModule,
     ServiceModule
   ],
   providers: [],
--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'
 
-import { SettingsService, SharedService, SidebarService } from './service.index';
+import { SettingsService, SharedService, SidebarService, UsuarioService, LoginGuardGuard } from './service.index';
 
 SettingsService
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
-  providers:[SettingsService, SharedService, SidebarService]
+  providers:[
+    SettingsService, 
+    SharedService, 
+    SidebarService, 
+    UsuarioService,
+    LoginGuardGuard
+  ]
 })
 export class ServiceModule { }
